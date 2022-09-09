1bc33c84e62801de248445a218f1addb650af435
Kshyshtoph <59285804+Kshyshtoph@users.noreply.github.com>
2022-09-09 19:54:58 +0200

    Update setup.sh

--- homework-2/gcp/scripts/setup.sh
+++ homework-2/gcp/scripts/setup.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt install -y nginx
-CONF=$(cat <<- END 
-events {
-    worker_connections 1024;
-}
-
-http {
-
-    server {
-        listen 80;
-        
-        location / {
-            root /www/data;
-        }
-    }
-}
-END
-)
-echo "$CONF" | sudo tee /etc/nginx/nginx.conf
-sudo mkdir /www 
-sudo mkdir /www/data 
-INDEX=$(cat <<- END
-<!doctype html>
-<html>
-  <head>
-    <title>Hello nginx</title>
-    <meta charset="utf-8" />
-  </head>
-  <body>
-    <h1>
-      Hello World!
-    </h1>
-  </body>
-</html>
-END
-)
-echo "$INDEX" | sudo tee /www/data/index.html
-sudo service nginx restart
+sudo apt update -y; sudo apt upgrade -y;
+sudo tasksel install desktop gnome-desktop -y
+sudo systemctl set-default graphical.target;
+sudo reboot;
